47be5ad5ac2818d6881054ca4030e4ed4b9b9ab4
zj-zhang <zj.z@ucla.edu>
2023-12-09 14:47:45 -0800

    update

--- amber/architect/modelSpace.py
+++ amber/architect/modelSpace.py
@@ -13,11 +13,11 @@ from __future__ import print_function
 from collections import defaultdict
 
 import numpy as np
-from ..backend import Operation, get_layer_shortname
-from .base import BaseModelSpace
+from ..backend import Operation
+from .base import BaseSearchSpace
 
 
-class ModelSpace(BaseModelSpace):
+class ModelSpace(BaseSearchSpace):
     """Model Space constructor
 
     Provides utility functions for holding "states" / "operations" that the controller must use to train and predict.
@@ -65,20 +65,6 @@ class ModelSpace(BaseModelSpace):
 
     def __setitem__(self, layer_id, layer_states):
         self.add_layer(layer_id, layer_states)
-
-    def get_space_size(self):
-        """Get the total model space size by the product of all candidate operations across all layers. No residual
-        connections are considered.
-
-        Returns
-        -------
-        size : int
-            The total number of possible combinations of operations.
-        """
-        size_ = 1
-        for i in self.state_space:
-            size_ *= len(self.state_space[i])
-        return size_
 
     def add_state(self, layer_id, state):
         """Append a new state/operation to a layer
@@ -203,52 +189,6 @@ class ModelSpace(BaseModelSpace):
                          for j in range(len(d[i]))])
         return ms
 
-
-class BranchedModelSpace(ModelSpace):
-    """
-    Parameters
-    ----------
-    subspaces : list
-        A list of `ModelSpace`. First element is a list of input branches. Second element is a stem model space
-    concat_op : str
-        string identifier for how to concatenate different input branches
-
-    """
-
-    def __init__(self, subspaces, concat_op='concatenate', **kwargs):
-        super().__init__(**kwargs)
-        self.subspaces = subspaces
-        self.concat_op = concat_op
-        # layer id to branch; expects a tuple of two elements
-        # first element is type index, 0=input branch, 1=stem
-        # second element is branch index, int=index of list, None=only one
-        # space present
-        self._layer_to_branch = {}
-        self._branch_to_layer = {}
-        # delineate subspaces
-        layer_id = 0
-        for i, model_space in enumerate(self.subspaces[0]):
-            for _layer in range(len(model_space)):
-                self.state_space[layer_id] = model_space[_layer]
-                self._layer_to_branch[layer_id] = (0, i)
-                layer_id += 1
-        for _layer in range(len(self.subspaces[1])):
-            self.state_space[layer_id] = self.subspaces[1][_layer]
-            self._layer_to_branch[layer_id] = (1, None)
-            layer_id += 1
-        for k, v in self._layer_to_branch.items():
-            if v in self._branch_to_layer:
-                self._branch_to_layer[v].append(k)
-            else:
-                self._branch_to_layer[v] = [k]
-
-    @property
-    def layer_to_branch(self):
-        return self._layer_to_branch
-
-    @property
-    def branch_to_layer(self):
-        return self._branch_to_layer
 
 
 # alias
